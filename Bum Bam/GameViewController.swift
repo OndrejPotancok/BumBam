e4ba9d880eaf59a1acd6e4437a82ca044e03dbfa
lukasbalaz <lukasbalaz7@gmail.com>
2015-07-06 17:12:55 +0200

    navigation

---
+++ Bum Bam/GameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GameViewController.swift
+//  Bum Bam
+//
+//  Created by Lukáš Baláž on 06/07/15.
+//  Copyright (c) 2015 TuppleApps. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func navigateBack(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+}
--- Bum Bam/MenuViewController.swift
+++ Bum Bam/MenuViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func menuButtonPressed(sender: AnyObject) {
+        performSegueWithIdentifier("startGame", sender: sender)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startGame" {
+            
+        }
+    }
 
 }
 
