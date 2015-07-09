620fa9c192584d850dd1aede2f5a26496b8e024d
Ondrej Potančok <ondrejpotancok@Mac-Pro.local>
2015-07-09 16:57:29 +0200

    added game design + settings design

--- Bum Bam/GuessShape/Layout/GuessShapeLayout.swift
+++ Bum Bam/GuessShape/Layout/GuessShapeLayout.swift
@@ -14,17 +14,73 @@ class GuessShapeLayout {
     
     var layout = Layout(
         subviews: [
-            "background": Layout(createView: { (prntW, prntH) -> UIView in
-                var view = UIView(frame: CGRectMake(0, 0, prntW, prntH))
-                view.backgroundColor = UIColor.greenColor()
-                return view
-            }),
-            "game": Layout(subviews: [
-                "siluette" : Layout(createView: { (prntW, prntH) -> UIView in
-                    var view = UIImageView(frame: CGRect(centerx: prntW/2, centery: prntH/3, width: min(prntW, prntH)*0.6, height: min(prntW, prntH)*0.6))
-                    view.image = UIImage(named: "GuessShapeCow")
+            "settings": Layout(
+                createView: { (prntW, prntH) -> UIView in
+                    var view = UIView(frame: CGRectMake(0, 0, prntW, prntH))
+                    view.backgroundColor = UIColor.redColor()
                     return view
-                })
+                },
+                subviews: [
+                    "selectShapeLabel": Layout(
+                        createView: { (prntW, prntH) -> UIView in
+                            var view = UILabel(frame: CGRect(centerx: prntW*0.5, centery: prntH*0.2, width: 200, height: 50))
+                            view.text = "shape to guess"
+                            view.textAlignment = NSTextAlignment.Center
+                            view.textColor = UIColor(red: 245, green: 245, blue: 245, alpha: 1.0)
+                            return view
+                    }),
+                    "selectDifficultyLabel": Layout(
+                        defaultHidden: false,
+                        createView: { (prntW, prntH) -> UIView in
+                            var view = UILabel(frame: CGRect(centerx: prntW*0.5, centery: prntH*0.5, width: 200, height: 50))
+                            view.text = "select difficulty"
+                            view.textAlignment = NSTextAlignment.Center
+                            view.textColor = UIColor(red: 245, green: 245, blue: 245, alpha: 1.0)
+                            return view
+                    }),
+                    "startGameButton": Layout(
+                        defaultHidden: false,
+                        createView: { (prntW, prntH) -> UIView in
+                            var view = UIButton(frame: CGRect(centerx: prntW*0.5, centery: prntH*0.8, width: 200, height: 50))
+                            view.setTitle("START GAME", forState: .Normal)
+                            view.setTitleColor(UIColor(red: 245, green: 245, blue: 245, alpha: 1.0), forState: .Normal)
+                            return view
+                    })
+                    
+                ]),
+            
+            "background": Layout(
+                createView: { (prntW, prntH) -> UIView in
+                    var view = UIView(frame: CGRectMake(0, 0, prntW, prntH))
+                    view.backgroundColor = UIColor.greenColor()
+                    return view
+            }),
+            "game": Layout(
+                subviews: [
+                    "siluette" : Layout(
+                        createView: { (prntW, prntH) -> UIView in
+                            var view = UIImageView(frame: CGRect(centerx: prntW/2, centery: prntH/3, width: min(prntW, prntH)*0.6, height: min(prntW, prntH)*0.6))
+                            view.image = UIImage(named: "GuessShapeSiluetteCow")
+                            return view
+                    }),
+                    "thumbCenter": Layout(
+                        createView: {(prntW, prntH) -> UIView in
+                            var view = UIImageView(frame: CGRect(centerx: prntW*0.5, centery: prntH-70, width: 100, height: 100))
+                            view.image = UIImage(named: "GuessShapeCow")
+                            return view
+                    }),
+                    "thumbLeft": Layout(
+                        createView: {(prntW, prntH) -> UIView in
+                            var view = UIImageView(frame: CGRect(centerx: prntW*0.2, centery: prntH-70, width: 100, height: 100))
+                            view.image = UIImage(named: "GuessShapeCow")
+                            return view
+                    }),
+                    "thumbRight": Layout(
+                        createView: {(prntW, prntH) -> UIView in
+                            var view = UIImageView(frame: CGRect(centerx: prntW*0.8, centery: prntH-70, width: 100, height: 100))
+                            view.image = UIImage(named: "GuessShapeCow")
+                            return view
+                    }),
                 ])
         ]
     )
