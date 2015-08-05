da63b2bed2e837f290c8bf3295b4434cd9a51b55
Ondrej Potančok <ondrejpotancok@Mac-Pro.local>
2015-08-05 17:08:51 +0200

    Added shaking of siluette on failure

--- Bum Bam/GuessShape/Layout/GuessShapeLayoutAction.swift
+++ Bum Bam/GuessShape/Layout/GuessShapeLayoutAction.swift
@@ -15,4 +15,20 @@ class GuessShapeLayoutAction: GuessShapeDefaultLayoutAction {
         self.layout["hud"]?.showSubview("success")
     }
     
+    override func failure() {
+        
+        var siluetteView = self.layout["game"]!["siluette"]!.view
+        var duration:NSTimeInterval = 0.15
+        UIView.animateKeyframesWithDuration(duration*2, delay: 0, options: UIViewKeyframeAnimationOptions(/*UIViewKeyframeAnimationOptions.Repeat.rawValue | */UIViewAnimationOptions.CurveLinear.rawValue), animations: { () -> Void in
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.25, animations: { () -> Void in
+                siluetteView.center = CGPointMake(siluetteView.center.x-10, siluetteView.center.y)
+            })
+            UIView.addKeyframeWithRelativeStartTime(0.25, relativeDuration: 0.5, animations: { () -> Void in
+                siluetteView.center = CGPointMake(siluetteView.center.x+20, siluetteView.center.y)
+            })
+            UIView.addKeyframeWithRelativeStartTime(0.75, relativeDuration: 0.25, animations: { () -> Void in
+                siluetteView.center = CGPointMake(siluetteView.center.x-10, siluetteView.center.y)
+            })
+        }, completion: nil)
+    }
 }
